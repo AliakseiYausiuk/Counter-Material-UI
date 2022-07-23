67372bab9119e1fd68d7289d9c56ced64bbf83d1
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-07-23 12:09:27 +0300

    Updates

---
+++ precache-manifest.106ec3e751c4ad7ea04473d96b5bf5ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f595d181b24670a3aab8084de6a85b",
+    "url": "/Counter-Material-UI/index.html"
+  },
+  {
+    "revision": "7794f7dd56a8d7a88ee5",
+    "url": "/Counter-Material-UI/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6fdf33f4abe264b7fbff",
+    "url": "/Counter-Material-UI/static/js/2.c9feeffc.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/Counter-Material-UI/static/js/2.c9feeffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7794f7dd56a8d7a88ee5",
+    "url": "/Counter-Material-UI/static/js/main.f8cf2e91.chunk.js"
+  },
+  {
+    "revision": "e6a50e47d2305fb4a2e4",
+    "url": "/Counter-Material-UI/static/js/runtime-main.b9cf1068.js"
+  }
+]);
